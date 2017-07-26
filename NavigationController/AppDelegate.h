b67db7d73cff464b71df344e680b8f8e4f19a337
Gotogoo <fcliang@thoughtworks.com>
2017-07-26 13:46:13 +0800

    Initial Commit

---
+++ NavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NavigationController
+//
+//  Created by Facheng Liang  on 26/07/2017.
+//  Copyright © 2017 Facheng Liang . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationController
+//
+//  Created by Facheng Liang  on 26/07/2017.
+//  Copyright © 2017 Facheng Liang . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
